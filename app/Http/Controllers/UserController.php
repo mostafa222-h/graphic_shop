4141afcd188891396d38c374c55004e2730eacd7
DESKTOP-RFHFMFO\Mostafa <mostafahekmatiii22@gmail.com>
2023-04-10 15:25:31 +0430

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        echo "This Is User Controller...my first activity in laravel after learn php language";
+    }
+}
