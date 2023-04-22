a90f6643afdf48ec20dd037b6130e2b1fb2e93a3
DESKTOP-RFHFMFO\Mostafa <mostafahekmatiii22@gmail.com>
2023-04-23 03:06:43 +0430

    categories section completly implemented

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 
 use App\Models\Category;
 use App\Http\Requests\Admin\Categories\StoreRequest;
+use App\Http\Requests\Admin\Categories\UpdateRequest;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
@@ -38,5 +39,33 @@ class CategoriesController extends Controller
         $categories = Category::paginate(10);
         //dd($categories);
         return view('admin.categories.all',compact('categories'));
+    }
+
+    public function delete($category_id)
+    {
+        $category = Category::find($category_id);
+        $category->delete();
+        return back()->with('success','دسته بندی حذف شد');
+    }
+    public function edit($category_id)
+    {
+        $category = Category::find($category_id);
+        return view('admin.categories.edit',compact('category'));
+       // $category->update();
+
+    }
+    public function update(UpdateRequest $request , $category_id)
+    {
+        $validatedData = $request->validated();
+        $category = Category::find($category_id);
+        $result = $category->update([
+            'title' => $validatedData['title'],
+            'slug' => $validatedData['slug']
+        ]);
+        if(!$result)
+        {
+            return back()->with('failed','به روز رسانی نشد');
+        }
+        return back()->with('success','به روز رسانی شد');
     }
 }
--- resources/views/admin/categories/all.blade.php
+++ resources/views/admin/categories/all.blade.php
@@ -25,6 +25,7 @@
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
+        @include('errors.message')
           <div class="row">
               <div class="col-12">
                   <div class="card">
@@ -58,8 +59,13 @@
                                   <td>{{ $category->title }}</td>
                                   <td>{{$category->created_at }}</td>
                                   <td>
-                                      <a href="#" class="btn btn-default btn-icons"><i class="fa fa-edit"></i></a>
-                                      <a href="#" class="btn btn-default btn-icons"><i class="fa fa-trash"></i></a>
+                                      <a href="{{ route('admin.categories.edit' , $category->id) }}" class="btn btn-default btn-icons"><i class="fa fa-edit"></i></a>
+                                      <form action="{{ route('admin.categories.delete', $category->id) }}" method="POST" style="display : inline">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-default btn-icons"><i class="fa fa-trash"></i></button>
+                                      </form>
+                                      
                                   </td>
                               </tr>
                               @endforeach
