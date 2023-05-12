693b23d295e5fec73a875fa6e741f45129f029fa
DESKTOP-RFHFMFO\Mostafa <mostafahekmatiii22@gmail.com>
2023-05-13 01:55:45 +0430

    user management implemented

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Users\StoreRequest;
+use App\Http\Requests\Admin\Users\UpdateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -15,25 +17,54 @@ class UsersController extends Controller
     }
     public function create()
     {
-        
+        return view('admin.users.add');
     }
-    public function store()
+    public function store(StoreRequest $request)
     {
+        $validatedData = $request->validated();
+        $createdUser =User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'mobile' => $validatedData['mobile'],
+            'role' => $validatedData['role']
+        ]);
+
+        if(!$createdUser)
+        {
+            return back()->with('failed','کاربر ایجاد نشد');
+        }
+        return back()->with('failed','کاربر ایجاد شد');
         
     }
 
-    public function edit()
+    public function edit($user_id)
     {
-        
+        $user = User::findOrFail($user_id);
+        return view('admin.users.edit',compact('user'));
     }
 
-    public function update()
+    public function update(UpdateRequest $request , $user_id)
     {
-        
+        $validatedData = $request->validated();
+        $user = User::findOrFail($user_id);
+        $result =$user->update([
+            'name' => $validatedData['name'] ,
+            'email' =>  $validatedData['email'] ,
+            'mobile' =>  $validatedData['mobile'] ,
+            'role' =>  $validatedData['role'] ,
+        ]);
+        if(!$result)
+        {
+            return back()->with('failed','کاربر به روز رسانی نشد.');
+        }
+            return back()->with('success','کاربر به روز رسانی شد.');
     }
 
-    public function delete()
+    public function delete($user_id)
     {
-        
+        $user = User::findOrFail($user_id);
+        $user->delete();
+
+        return back()->with('success','کاربر حذف شد.');
     }
 }
