01514d32f8c25182815769d580f933178e5dd15c
DESKTOP-RFHFMFO\Mostafa <mostafahekmatiii22@gmail.com>
2023-05-15 23:37:55 +0330

    filter implemented

--- app/Http/Controllers/Home/ProductsController.php
+++ app/Http/Controllers/Home/ProductsController.php
@@ -9,11 +9,40 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::all();
+        $products = null ;
+        if(isset($request?->filter,$request?->action))
+        {
+         $products = $this->findFilter($request?->filter,$request?->action) ?? Product::all();
+        }elseif($request->has('search'))
+        {
+            $products = Product::where('title', 'LIKE' , '%' . $request->input('search') . '%')->get();
+        }
+        else{
+            $products = Product::all();
+        }
+        
         $categories  = Category::all();
         return view('frontend.products.all',compact('products','categories'));
+    }
+    private function findFilter(string $className , string $methodName)
+    {
+        $baseNameSpace = "App\Http\Controllers\Filters\\";
+       
+        $className = $baseNameSpace .(ucfirst($className) . 'Filter') ;
+        if(!class_exists($className))
+        {
+            return null ;
+        }
+        $object = new $className;
+
+        if(!method_exists($object,$methodName))
+        {
+            return null ;
+        }
+        return $object->{$methodName}();
+
     }
     public function show($product_id)
     {
